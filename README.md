be649c472981f092ed4a277a17f2c040a45bed7b
qgorak <q.gorak@gmail.com>
2021-05-18 12:39:48 +0200

    graph moyenne valeur foncière par departement

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # projetImmo
  
+ ## Prerequisites
+
+ You will need the following things properly installed on your computer.
+
+ * python^3.6
+ * django^2.2
+ * pandas
--- graph/views.py
+++ graph/views.py
@@ -5,11 +5,23 @@ import json
 
 
 def index(request):
-    df = pd.read_csv("valeursfoncieres-2020.txt",sep='|')
+    # On precise en argument le séparateur des décimal ( pandas utilise "." )
+    # On precise également un converter pour que le code département soit traité comme un string ( pour ne pas perdre les 0 pour les departement de 01 à 09 )
+    df = pd.read_csv("valeursfoncieres-2020.txt",sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
     df.head()
-    prix = df[['Valeur fonciere','Code postal']].groupby('Code postal').count().sort_values(by='Valeur fonciere', ascending=False)
-    prix.reset_index(0, inplace=True)
-    prix.head()
-    data= '[ 1 , 2 , 3 , 4]'
-    context = {'data': data, 'prix':prix['Code postal'] }
+
+    # On groupe par code departement et on fait la moyenne de la colonne par departement
+    moyennevfpardep = df.groupby(['Code departement'])['Valeur fonciere'].mean().reset_index()
+
+    # On renomme les colonnes pour convenir à highcharts
+    moyennevfpardep.rename(columns={'Code departement': 'name'}, inplace=True)
+    moyennevfpardep.rename(columns={'Valeur fonciere': 'y'}, inplace=True)
+
+    # puis on format en json
+    moyennevfpardep = moyennevfpardep.to_json(orient="records")
+
+    # On prépare les variables que l'on va passer à la vue
+    context = {'data': '', 'test':'','valeurfoncierepardep':moyennevfpardep }
+
+    # On charge la vue avec les paramètres
     return render(request, 'graph/index.html', context)
