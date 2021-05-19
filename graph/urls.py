d380171edd946d24a63d19fe9c158013e296b7b6
qgorak <q.gorak@gmail.com>
2021-05-19 11:38:36 +0200

    Rechercher une commune / rechercher un departement / france entiere / a propos

--- graph/urls.py
+++ graph/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('searchdep', views.searchdep, name='searchdep'),
+    path('searchcommune', views.searchcommune, name='searchcommune'),
+    path('france', views.france, name='france'),
+    path('departement/<int:dep_id>', views.departement, name='departement'),
+    path('commune/<slug:commune>', views.commune, name='commune'),
+    path('about', views.about, name='about'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -2,26 +2,82 @@ from django.shortcuts import render
 import pandas as pd
 import numpy as np
 import json
+# datetime oprations
+from datetime import timedelta
+# to get web contents
+from urllib.request import urlopen
+# basic visualization package
+import matplotlib.pyplot as plt
+
+# interactive visualization
+import plotly.express as px
+import plotly.graph_objs as go
+# import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
+# basic visualization package
+import matplotlib.pyplot as plt
+
+
+# interactive visualization
+import plotly.express as px
+import plotly.graph_objs as go
+# import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
 
 
 def index(request):
-    # On precise en argument le séparateur des décimal ( pandas utilise "." )
-    # On precise également un converter pour que le code département soit traité comme un string ( pour ne pas perdre les 0 pour les departement de 01 à 09 )
+    return render(request, 'graph/index.html')
+
+def france(request):
+    df = pd.read_csv("valeursfoncieres-2020.txt", low_memory=False,sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
+    df.head()
+    moyennevfpardep = df.groupby(['Code departement'])['Valeur fonciere'].mean().reset_index()
+    fig = px.bar(moyennevfpardep, x='Code departement', y='Valeur fonciere', height=600, width=700,
+             title='Moyenne de la valeur foncière des ventes par département')
+    graph = fig.to_html
+    # On prépare les variables que l'on va passer à la vue
+    context = {'figure1': graph }
+    return render(request, 'graph/france.html', context)
+
+def searchdep(request):
+    return render(request, 'graph/searchdep.html')
+
+def searchcommune(request):
+    return render(request, 'graph/searchcommune.html')
+
+
+def departement(request,dep_id):
+    dep_id = str(dep_id)
     df = pd.read_csv("valeursfoncieres-2020.txt",sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
     df.head()
+    df = df.loc[df['Code departement'] == dep_id]
+    vente = df
+    vente['Nombre de vente'] = vente.groupby('Date mutation')['Commune'].transform('count')
+    vente['Date mutation'] = pd.to_datetime(vente['Date mutation'])
+    vente.sort_values(by=['Date mutation'], inplace=True, ascending=False)
+    vente.head()
+    figure1 = px.line(vente, x="Date mutation", y="Nombre de vente", width=700)
+    figure1.update_layout(title='Nombre de vente par jour ', xaxis_title="", yaxis_title="")
+    figure1 = figure1.to_html
+    context = {'codedep':dep_id,'figure1':figure1}
+    return render(request, 'graph/departement.html', context)
 
-    # On groupe par code departement et on fait la moyenne de la colonne par departement
-    moyennevfpardep = df.groupby(['Code departement'])['Valeur fonciere'].mean().reset_index()
+def commune(request,commune):
+    df = pd.read_csv("valeursfoncieres-2020.txt",sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
+    df.head()
+    df = df.loc[df['Commune'] == commune]
+    vente = df
+    vente['Nombre de vente'] = vente.groupby('Date mutation')['Commune'].transform('count')
+    vente['Date mutation'] = pd.to_datetime(vente['Date mutation'])
+    vente.sort_values(by=['Date mutation'], inplace=True, ascending=False)
+    vente.head()
+    figure1 = px.line(vente, x="Date mutation", y="Nombre de vente", width=700)
+    figure1.update_layout(title='Nombre de vente par jour ', xaxis_title="", yaxis_title="")
+    figure1 = figure1.to_html
+    context = {'commune':commune,'figure1':figure1}
+    return render(request, 'graph/commune.html', context)
 
-    # On renomme les colonnes pour convenir à highcharts
-    moyennevfpardep.rename(columns={'Code departement': 'name'}, inplace=True)
-    moyennevfpardep.rename(columns={'Valeur fonciere': 'y'}, inplace=True)
-
-    # puis on format en json
-    moyennevfpardep = moyennevfpardep.to_json(orient="records")
-
-    # On prépare les variables que l'on va passer à la vue
-    context = {'data': '', 'test':'','valeurfoncierepardep':moyennevfpardep }
-
-    # On charge la vue avec les paramètres
-    return render(request, 'graph/index.html', context)
+def about(request):
+    return render(request, 'graph/about.html')
