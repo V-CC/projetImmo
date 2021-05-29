2aa36e465897ead8f0dc95a088a4934a95354abd
V-CC <77583878+V-CC@users.noreply.github.com>
2021-05-29 16:26:32 +0200

    ne

--- graph/views.py
+++ graph/views.py
@@ -15,28 +15,25 @@ import plotly.graph_objs as go
 # import plotly.figure_factory as ff
 from plotly.subplots import make_subplots
 
-# basic visualization package
-import matplotlib.pyplot as plt
-
-
-# interactive visualization
-import plotly.express as px
-import plotly.graph_objs as go
-# import plotly.figure_factory as ff
-from plotly.subplots import make_subplots
-
-
-
 def index(request):
     return render(request, 'graph/index.html')
 
 def france(request):
     df = pd.read_csv("valeursfoncieres-2020.txt", low_memory=False,sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
-    df.head()
+    # Find the columns where each value is null
+    cols_vides = [col for col in df.columns if df[col].isnull().all()]
+    # Drop these columns from the dataframe
+    df.drop(cols_vides,
+        axis=1,
+        inplace=True)
+    df = df.fillna(0)
+
     moyennevfpardep = df.groupby(['Code departement'])['Valeur fonciere'].mean().reset_index()
     fig = px.bar(moyennevfpardep, x='Code departement', y='Valeur fonciere', height=600, width=700,
-             title='Moyenne de la valeur foncière des ventes par département')
+            title='Moyenne de la valeur foncière des ventes par département')
+    
     graph = fig.to_html
+    #graph.head(100)
     # On prépare les variables que l'on va passer à la vue
     context = {'figure1': graph }
     return render(request, 'graph/france.html', context)
@@ -51,21 +48,32 @@ def searchcommune(request):
 def departement(request,dep_id):
     dep_id = str(dep_id)
     df = pd.read_csv("valeursfoncieres-2020.txt",sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
-    df.head()
+    cols_vides = [col for col in df.columns if df[col].isnull().all()]
+    # Drop these columns from the dataframe
+    df.drop(cols_vides,
+        axis=1,
+        inplace=True)
+    df = df.fillna(0)
     df = df.loc[df['Code departement'] == dep_id]
-    vente = df
-    vente['Nombre de vente'] = vente.groupby('Date mutation')['Commune'].transform('count')
-    vente['Date mutation'] = pd.to_datetime(vente['Date mutation'])
-    vente.sort_values(by=['Date mutation'], inplace=True, ascending=False)
-    vente.head()
-    figure1 = px.line(vente, x="Date mutation", y="Nombre de vente", width=700)
+    df['Nombre de vente'] = df.groupby('Date mutation')['Commune'].transform('count')
+    df['Date mutation'] = pd.to_datetime(df['Date mutation'])
+    df.sort_values(by=['Date mutation'], inplace=True, ascending=False)
+    df = df.loc[df['Type local'] == 'Maison']
+    df['Prix metre carre'] = df.apply(lambda x: x['Valeur fonciere']/x['Surface reelle bati'] if x['Surface reelle bati'] > 0 else 0, axis=1)
+    df = df[df['Prix metre carre'] >0]
+
+    moyenneMetreCarreJour = df.groupby(['Date mutation'])['Prix metre carre'].mean().reset_index()
+
+    figure1 = px.line(df, x="Date mutation", y="Nombre de vente", width=700)
     figure1.update_layout(title='Nombre de vente par jour ', xaxis_title="", yaxis_title="")
+    figure2 = px.line(moyenneMetreCarreJour,title='Prix metre carre', x="Date mutation", y="Prix metre carre", width=700)
     figure1 = figure1.to_html
-    context = {'codedep':dep_id,'figure1':figure1}
+    figure2 = figure2.to_html
+    context = {'codedep':dep_id,'figure1':figure1,'figure2':figure2}
     return render(request, 'graph/departement.html', context)
 
 def commune(request,commune):
-    df = pd.read_csv("valeursfoncieres-2020.txt",sep='|', decimal=',', converters={'Code departement': lambda x: str(x)})
+    df = pd.read_csv("valeursfoncieres-2020.txt",sep='|' , decimal=',', converters={'Code departement': lambda x: str(x)})
     df.head()
     df = df.loc[df['Commune'] == commune]
     vente = df
